6e3d31362e89e60c5d05220060e3c98d5d638c9a
davidelamorte <lamortedavide@gmail.com>
2020-10-12 21:31:24 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const io = require("socket.io")();
+const shortid = require("shortid");
+const data = require("./data.json");
+const cloneDeep = require("clone-deep");
+
+const port = process.env.PORT || 5000;
+const state = {};
+const clientRooms = {};
+
+io.on("connection", (client) => {
+  const handleNewGame = () => {
+    let roomName = shortid.generate();
+    clientRooms[client.id] = roomName;
+    state[roomName] = cloneDeep(data);
+    client.join(roomName);
+    client.number = 1;
+
+    client.emit("gameStarted", {
+      gameData: state[roomName],
+      player: 1,
+      roomName: roomName,
+    });
+  };
+
+  const handleJoinGame = (roomName) => {
+    if (!io.sockets.adapter.rooms[roomName]) return;
+    clientRooms[client.id] = roomName;
+    client.join(roomName);
+    client.number = 2;
+    client.emit("gameStarted", {
+      gameData: state[roomName],
+      player: 2,
+      roomName: roomName,
+    });
+
+    io.sockets.in(roomName).emit("playerJoined");
+  };
+
+  const handleGameChange = (change) => {
+    const roomName = clientRooms[client.id];
+    state[roomName].boxes[change.key].owner = change.player;
+    const endGameResults = handleEndGame(state[roomName].boxes);
+
+    if (endGameResults) {
+      io.sockets.in(roomName).emit("endGame", {
+        results: endGameResults,
+      });
+    } else {
+      io.sockets.in(roomName).emit("gameChange", {
+        gameData: state[roomName],
+      });
+    }
+  };
+
+  const handleEndGame = (boxes) => {
+    const remainingBoxes = boxes.filter((el) => el.owner === 0);
+    console.log(remainingBoxes.length);
+    if (remainingBoxes.length) return false;
+    return calculateResult(boxes);
+  };
+
+  const calculateResult = (boxes) => {
+    const result = {};
+    for (const box of boxes) {
+      result[box.owner] = (result[box.owner] || 0) + 1;
+    }
+    return result;
+  };
+
+  client.on("newGame", handleNewGame);
+  client.on("joinGame", handleJoinGame);
+  client.on("gameChange", handleGameChange);
+});
+
+io.listen(port);
